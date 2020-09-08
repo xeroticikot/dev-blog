6f6d86a0daa448f08d8b7f53d3065555071f4c3f
Mahmud Abdur Rahman <clanlord.ikot@gmail.com>
2020-09-09 00:52:57 +0600

    Updates

---
+++ precache-manifest.aef8754d7052c7975a3a4258c5156c15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0294ba0892b356f180e0e3f27679ceae",
+    "url": "/dev-blog/index.html"
+  },
+  {
+    "revision": "38503dd4513ea28a69b8",
+    "url": "/dev-blog/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "384fee8f2c599a247422",
+    "url": "/dev-blog/static/css/main.0dd37de9.chunk.css"
+  },
+  {
+    "revision": "38503dd4513ea28a69b8",
+    "url": "/dev-blog/static/js/2.5a60d349.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/dev-blog/static/js/2.5a60d349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384fee8f2c599a247422",
+    "url": "/dev-blog/static/js/main.fd627b70.chunk.js"
+  },
+  {
+    "revision": "ed72a80c657501cce75d",
+    "url": "/dev-blog/static/js/runtime-main.a06eb3f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dev-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
